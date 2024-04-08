ebb3cfac4fbdf9141a27392744e24428f031bb8a
Vinicius <jorgevinicius2002@hotmail.com>
2024-04-08 16:38:19 -0300

    Modificação futura

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,10 +1,16 @@
-const NotesContent = ["Colocar albums para as fotos"];
+const NotesContent = [
+  "Colocar albums para as fotos",
+  "Fazer função de download de imagem",
+];
 
 export const Notes = () => {
   return (
     <ul>
       {NotesContent.map((note, i) => (
-        <li key={i} className="w-full bg-yellow-200 text-black p-2 rounded shadow">
+        <li
+          key={i}
+          className="w-full bg-yellow-200 text-black p-2 rounded shadow"
+        >
           {note}
         </li>
       ))}
